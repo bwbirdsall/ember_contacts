f24918a0e5d5bce969fb64553c9fe9c2846f9a31
Ben Birdsall & robin miller <student@epicodus.com>
2014-04-28 09:09:52 -0700

    initial commit of json api, ready for ember.js.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BinderFullOfPeople::Application.config.secret_key_base = '2c1561e82a4664f7495062a0e75d24f7d8a2c9927e3c6a47c16afc77fa7aa1f288488a0ca0654de166bea821ad832156950489a8f5069688e10eacee1f97559e'
